ac5145a4cc102253e151872db56ae272c962215f
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-11 21:14:31 -0700

    Create playRound Function
    This functions compares the choices between the
    human and computer and checks to see who is the
    winner

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Gets randomly generated choice from computer
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     console.log(num);
@@ -13,8 +14,48 @@ function getComputerChoice() {
     
 }
 
+// Gets input from user
 function getHumanChoice() {
     return prompt("Enter rock, paper, or scissors: ");
+}
+
+// Checks who is the winner between the two players
+function playRound(humanChoice, computerChoice) {
+    let choice = humanChoice.toLowerCase();
+
+    switch (choice){
+        case "rock":
+            if (computerChoice === "paper"){
+                console.log("Computer Wins!");
+                computerScore += 1;
+            } else if (computerChoice === "scissors"){
+                console.log("You Win!");
+                humanScore += 1;
+            } else if (computerChoice === "rock"){
+                console.log("Draw!")
+            }
+        case "paper":
+            if (computerChoice === "paper"){
+                console.log("Draw");
+            } else if (computerChoice === "scissors"){
+                console.log("Computer Win!");
+                computerScore += 1;
+            } else if (computerChoice === "rock"){
+                console.log("You Win!")
+                humanScore += 1
+            }
+        case "scissors":
+            if (computerChoice === "paper"){
+                console.log("You Win!");
+                humanScore += 1;
+            } else if (computerChoice === "scissors"){
+                console.log("Draw!");
+            } else if (computerChoice === "rock"){
+                console.log("Computer Wins!")
+                computerScore += 1;
+            }
+    }
+
 }
 
 
