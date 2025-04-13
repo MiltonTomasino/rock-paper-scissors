22eeb5ba5d4819d10baee64a03c14c0b45623600
MiltonTomasino <jonathantomasino13@gmail.com>
2025-04-13 15:06:34 -0700

    Add Basic UI Interactivity to the Game
    This makes the game playable without the console
    and now uses a basic user interface. This makes it
    easier for the user to know what is happening.

--- javascript.js
+++ javascript.js
@@ -1,64 +1,73 @@
 // Gets randomly generated choice from computer
 function getComputerChoice() {
-    let num = Math.floor(Math.random() * 3);    
+    let num = Math.floor(Math.random() * 3);
 
-    if (num === 0){
+    if (num === 0) {
         return "rock";
-    } else if (num === 1){
+    } else if (num === 1) {
         return "paper";
     } else {
         return "scissors";
     }
-    
-}
 
-// Gets input from user
-function getHumanChoice() {
-    return prompt("Enter rock, paper, or scissors: ");
 }
 
 // Checks who is the winner between the two players
 function playRound(humanChoice, computerChoice) {
     let choice = humanChoice.toLowerCase();
 
-    switch (choice){
+    switch (choice) {
         case "rock":
-            if (computerChoice === "paper"){
+            if (computerChoice === "paper") {
                 console.log("Computer Wins!");
                 computerScore += 1;
-            } else if (computerChoice === "scissors"){
+            } else if (computerChoice === "scissors") {
                 console.log("You Win!");
                 humanScore += 1;
-            } else if (computerChoice === "rock"){
+            } else if (computerChoice === "rock") {
                 console.log("Draw!")
             }
             break;
         case "paper":
-            if (computerChoice === "paper"){
+            if (computerChoice === "paper") {
                 console.log("Draw");
-            } else if (computerChoice === "scissors"){
+            } else if (computerChoice === "scissors") {
                 console.log("Computer Win!");
                 computerScore += 1;
-            } else if (computerChoice === "rock"){
+            } else if (computerChoice === "rock") {
                 console.log("You Win!")
                 humanScore += 1
             }
             break;
         case "scissors":
-            if (computerChoice === "paper"){
+            if (computerChoice === "paper") {
                 console.log("You Win!");
                 humanScore += 1;
-            } else if (computerChoice === "scissors"){
+            } else if (computerChoice === "scissors") {
                 console.log("Draw!");
-            } else if (computerChoice === "rock"){
+            } else if (computerChoice === "rock") {
                 console.log("Computer Wins!")
                 computerScore += 1;
             }
             break;
         default:
             console.log("Invalid input");
-            break;    
-    }    
+            break;
+    }
+
+    playerScoreBoard.innerHTML = `Player: ${humanScore}`;
+    computerScoreBoard.innerHTML = `Computer: ${computerScore}`
+
+    if (humanScore === 5 || computerScore === 5) {
+        btn1.disabled = true;
+        btn2.disabled = true;
+        btn3.disabled = true;
+        restart.classList.add("active");
+        let winner = humanScore === 5 ? "You" : "Computer";
+        winnerHeader.textContent = `The winner is: ${winner}!`
+        restart.appendChild(winnerHeader)
+        
+    }
 
 }
 
@@ -66,22 +75,52 @@ function playRound(humanChoice, computerChoice) {
 let humanScore = 0;
 let computerScore = 0;
 
+// collect necessary elements
+const btnContainer = document.querySelector(".btn-container");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const restart = document.querySelector(".restart");
+const playerScoreBoard = document.querySelector(".player");
+const computerScoreBoard = document.querySelector(".computer");
+const winnerHeader = document.createElement("h1");
 
-// start the game and play for roundCount times
-function playGame() {
-    let roundCount = 5;
+// retrieve player's choice and pass it to playRound
+btnContainer.addEventListener("click", (e) => {
+    let target = e.target;
+    let computer = getComputerChoice();
 
-    while (roundCount > 0){
-        let human = getHumanChoice();
-        let computer = getComputerChoice();
-        
-        console.log(`You chose ${human} and the computer chose ${computer}`);
-
-        playRound(human, computer);
-
-        console.log(`SCORE:\nHuman: ${humanScore} Computer: ${computerScore}`);
-
-        roundCount -= 1;
+    switch (target.id) {
+        case "btn1":
+            console.log("Rock");
+            playRound("rock", computer);
+            break;
+        case "btn2":
+            console.log("Paper");
+            playRound("paper", computer);
+            break;
+        case "btn3":
+            console.log("Scissors");
+            playRound("scissors", computer);
+            break;
     }
+});
+
+restart.addEventListener("click", () => {
+    restartGame();
+})
+
+
+// restart game
+function restartGame() {
+    humanScore = 0;
+    computerScore = 0;
+    playerScoreBoard.textContent = "Player: 0";
+    computerScoreBoard.textContent = "Computer: 0";
+    btn1.disabled = false;
+    btn2.disabled = false;
+    btn3.disabled = false;
+    restart.classList.remove("active");
+    restart.removeChild(winnerHeader);
 }
 
